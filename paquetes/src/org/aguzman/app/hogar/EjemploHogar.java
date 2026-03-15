717a91a9b545d9bf692b3387e74f13c24d5cb77f
Xabier <shao.capilla@gmail.com>
2026-03-16 00:31:15 +0100

    import static

---
+++ paquetes/src/org/aguzman/app/hogar/EjemploHogar.java
@@ -0,0 +1,11 @@
+package org.aguzman.app.hogar;
+
+import org.aguzman.app.jardin.Perro;
+
+public class EjemploHogar {
+    public static void main(String[] args) {
+        Persona p = new Persona();
+        Perro p1 = new Perro();
+        String saludo = Persona.saludar();
+    }
+}
