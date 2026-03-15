67a06315bc62435bac9e93f32c2508e4ff4a33ea
Xabier <shao.capilla@gmail.com>
2026-03-15 12:28:40 +0100

    crado las clases relacionales

---
+++ poo/src/Tanque.java
@@ -0,0 +1,16 @@
+public class Tanque {
+
+    private  int capacidad;
+
+    public Tanque() {
+        this.capacidad = 40;
+    }
+
+    public Tanque(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+}
